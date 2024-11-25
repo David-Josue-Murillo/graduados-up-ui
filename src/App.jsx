18d7e947f4e0195f72eea08a70ccd443b9e12b06
David Murillo <david.josue.murillo23@gmail.com>
2024-11-25 10:40:36 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+    return (
+        <div className="text-center mt-2">
+            <h1 className="text-4xl text-yellow-700 font-bold underline">Graduados UP</h1>
+            <p className="text-xl">By David Murillo</p>
+        </div>
+    )
+}
+
+export default App
