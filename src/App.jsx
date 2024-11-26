8945c5e96f0c0a839bd5660be029a4013b1deed8
David Murillo <david.josue.murillo23@gmail.com>
2024-11-25 23:13:39 -0500

    style: :building_construction: Defining the files structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import Home from "./pages/Home"
 
 function App() {
 
     return (
-        <div className="text-center mt-2">
-            <h1 className="text-4xl text-yellow-700 font-bold underline">Graduados UP</h1>
-            <p className="text-xl">By David Murillo</p>
-        </div>
+        <>
+            <Home />
+        </>
     )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <>
+            <h1 className="text-lg font-bold">Graduates</h1>
+        </>
+    );
+};
+
+export default Home;
