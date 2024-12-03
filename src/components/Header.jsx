d0007f413e6d25f0958b87cb91d76b27388caefc
David Murillo <david.josue.murillo23@gmail.com>
2024-12-02 21:04:23 -0500

    style: :lipstick: Components Responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/img/logo_light.png'
+import { Link } from 'react-scroll'
+import { CiMenuFries } from 'react-icons/ci';
+import { FaTimes } from 'react-icons/fa';
 
 const Header = () => {
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
     return (
         <>
-            <header className="flex justify-between items-center py-4 px-12 bg-gray-50 border-b">
-                <div className='"w-20 h-20 overflow-hidden'>
+            <header className="flex justify-between items-center flex-wrap py-4 px-6 md:px-12 bg-gray-50 border-b">
+                <div className='w-5/12 md:w-1/6  overflow-hidden'>
                     <img src={logo} alt="Logo" className='w-full h-full object-cover' />
                 </div>
-                <nav className='mr-32'>
-                    <ul className="flex space-x-8 font-medium mt-5 text-md font-farro">
-                        <li><a href="#home" className="hover:text-green-600">Inicio</a></li>
-                        <li><a href="#docs" className="hover:text-green-600">Documentación</a></li>
-                        <li><a href="#examples" className="hover:text-green-600">Ejemplos</a></li>
-                        <li><a href="#up" className="hover:text-green-600">Up</a></li>
+
+                <button className='block sm:hidden transition' onClick={handleClick}>
+                    {click ? <FaTimes /> : <CiMenuFries />}
+                </button>
+
+                <nav className={ click ? 'flex w-full text-center' : 'hidden md:block md:mr-32'}>
+                    <ul className="w-full px-5 mt-5 space-x-8  font-farro text-sm font-medium md:flex md:text-base">
+                        <Link spy={true} smooth={true} to='Home'>
+                            <li className='hover:text-green-600'>Inicio</li>
+                        </Link>
+                        <Link spy={true} smooth={true} to='Documentation'>
+                            <li className='hover:text-green-600'>Documentación</li>
+                            </Link>
+                        <Link spy={true} smooth={true} to='Examples'>
+                            <li className='hover:text-green-600'>Ejemplos</li>
+                            </Link>
+                        <Link spy={true} smooth={true} to='Up'>
+                            <li className='hover:text-green-600'>Up</li>
+                            </Link>
                     </ul>
                 </nav>
-                <div className="flex items-center space-x-6 mt-1">
+                
+                <div className="items-center space-x-6 mt-1 hidden md:flex">
                     <div className='hover:scale-110'>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            width="24"
-                            height="24"
-                            stroke-width="2">
-
-                            <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path>
-                            <path d="M17 4a2 2 0 0 0 2 2a2 2 0 0 0 -2 2a2 2 0 0 0 -2 -2a2 2 0 0 0 2 -2"></path>
-                            <path d="M19 11h2m-1 -1v2"></path>
-                        </svg>
+                        <Link to=''>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                width="24"
+                                height="24"
+                                stroke-width="2">
+                                <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path>
+                                <path d="M17 4a2 2 0 0 0 2 2a2 2 0 0 0 -2 2a2 2 0 0 0 -2 -2a2 2 0 0 0 2 -2"></path>
+                                <path d="M19 11h2m-1 -1v2"></path>
+                            </svg>
+                        </Link>
                     </div>
                     <div className='hover:scale-110'>
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 30 30">
-                            <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
-                        </svg>
+                        <Link to=''>
+                            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 30 30">
+                                <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
+                            </svg>
+                        </Link>
                     </div>
                 </div>
             </header>
