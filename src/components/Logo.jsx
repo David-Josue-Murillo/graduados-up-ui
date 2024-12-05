a08418548d538147559350e08338ca890bdedcec
David Murillo <david.josue.murillo23@gmail.com>
2024-12-04 23:35:45 -0500

    feat: :recycle: Splitting code into components

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,5 @@
 import logo from '../assets/img/logo_light.png'
 
-
 const Logo = () => {
     return (
         <div className='w-5/12 sm:w-3/12 md:w-2/12 lg:w-1/6  overflow-hidden' data-aos="fade-right">
