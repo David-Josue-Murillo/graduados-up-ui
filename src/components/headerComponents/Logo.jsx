11664091848b5537d8eb69e36cb5be4fc8af4c81
David Murillo <david.josue.murillo23@gmail.com>
2024-12-16 21:03:13 -0500

    style: :lipstick: Working with responsive design

--- src/components/headerComponents/Logo.jsx
+++ src/components/headerComponents/Logo.jsx
@@ -3,7 +3,7 @@ import logo_dark from '../../assets/img/logo_dark.png'
 
 const Logo = ({theme}) => {
     return (
-        <div className='w-5/12 sm:w-3/12 md:w-2/12 lg:w-1/6  overflow-hidden' data-aos="fade-right">
+        <div className='w-5/12 sm:w-3/12 lg:w-1/6  overflow-hidden' data-aos="fade-right">
             <img src={theme === 'light' ? logo_light : logo_dark} alt="Logo" className='w-full h-full object-cover' />
         </div>
     )
--- src/components/headerComponents/Navbar.jsx
+++ src/components/headerComponents/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Navbar = ({click}) => {
     return (
         <nav className={click ? 'flex w-full text-center mt-5' : 'hidden md:block lg:mr-32'}>
-            <ul className="w-full px-5 mt-5 space-x-8  font-farro text-sm font-medium md:mt-3 md:flex md:text-xs lg:mt-5 lg:text-base cursor-pointer">
+            <ul className="w-full px-5 mt-5 space-x-8  font-farro text-sm font-medium md:mt-3 md:flex lg:mt-5 lg:text-base cursor-pointer">
                 <Link to='/'>
                     <li className='hover:text-green-600'>Inicio</li>
                 </Link>
