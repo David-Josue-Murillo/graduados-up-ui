6a40ef3e5a8306752fc56a4300384b26264e0443
David Murillo <david.josue.murillo23@gmail.com>
2025-01-01 18:17:16 -0500

    style: :iphone: Working on a responsive design

--- src/pages/About/components/ProfileSections.jsx
+++ src/pages/About/components/ProfileSections.jsx
@@ -1,15 +1,15 @@
 const ProfileSection = ({ image, title, description }) => (
-    <section className="px-12 mt-12 flex gap-4">
-        <div className="w-3/6">
+    <section className="lg:flex lg:px-12 mt-12 gap-4">
+        <div className="w-5/6 lg:w-3/6 mx-auto mb-4 md:mb-0">
             <img
                 src={image}
                 alt="Profile"
-                className="w-44 h-44 rounded-full object-cover"
+                className="w-44 h-44 mx-auto rounded-full object-cover"
                 loading="lazy"
             />
         </div>
         <div className="my-auto">
-            <h1 className="text-green-600 text-2xl font-farro-bold font-bold">
+            <h1 className="text-green-600 text-2xl text-center lg:text-start mt-4 lg:mt-0 font-farro-bold font-bold">
                 {title}
             </h1>
             <p className="text-gray-700 font-farro dark:text-gray-300 text-sm mt-4">
--- src/pages/About/components/TextSection.jsx
+++ src/pages/About/components/TextSection.jsx
@@ -1,11 +1,11 @@
 const TextSection = ({ title, content }) => (
-    <section className="px-12">
+    <section className="lg:px-12">
         <h1 className="text-green-600 text-center text-3xl font-farro-bold font-bold">
             {title}
         </h1>
         {Array.isArray(content) ? (
             content.map((paragraph, index) => (
-                <p key={index} className="text-gray-700 font-farro dark:text-gray-300 mt-4">
+                <p key={index} className="text-gray-700 text-sm md:text-md font-farro dark:text-gray-300 mt-4">
                     {paragraph}
                 </p>
             ))
