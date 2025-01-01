e901bd99ad763f9ef68624f5eb4b034d81265522
David Murillo <david.josue.murillo23@gmail.com>
2025-01-01 17:41:31 -0500

    fix: :bug: invalidated reference fixed

--- src/pages/Documentation/components/doc/EndpointsSection.jsx
+++ src/pages/Documentation/components/doc/EndpointsSection.jsx
@@ -1,27 +1,16 @@
-import SyntaxBlock from '../../../../components/SyntaxBlock';
 import SectionHeader from './SectionHeader';
-import InfoRespComponent from '../../../../components/endpointComponents/InfoRespComponent';
 import campusResponse from '../../../../mocks/api/campusResponse.json'
-import facultyResponse from '../../../../mocks/api/facultyResponse.json'
 import careersResponse from '../../../../mocks/api/careersResponse.json'
 import graduateResponse from '../../../../mocks/api/graduatesResponse.json'
-import InforEndpoint from '../../../../components/endpointComponents/InforEndpoint';
+import facultyResponse from '../../../../mocks/api/facultyResponse.json'
 import { ENDPOINTS_DATA } from '../../constants/endpoints';
 import EndpointSection from '../../../../components/EndpointSection';
 
-
 const EndpointsSection = ({graduatesRef, careersRef, facultiesRef, campusRef}) => {
-
-    const filtersGraduates = [
-        ['Año', 'year', '2023'],
-        ['Carrera', 'career_id', 'Ingeniería en Informática'],
-        ['Campus', 'campus_id', 'Campus Central'],
-    ];
-
     const endpointResponse = {
         graduates: graduateResponse,
         careers: careersResponse,
-        facultiesRef: facultyResponse,
+        faculty: facultyResponse,
         campus: campusResponse
     }
 
@@ -34,7 +23,7 @@ const EndpointsSection = ({graduatesRef, careersRef, facultiesRef, campusRef}) =
             />
 
             {Object.entries(ENDPOINTS_DATA).map(([key, value]) => (
-                <EndpointSection 
+                <EndpointSection
                     key={key}
                     id={key}
                     reference={key === 'graduates' ? graduatesRef : key === 'careers' ? careersRef : key === 'faculty' ? facultiesRef : campusRef}
