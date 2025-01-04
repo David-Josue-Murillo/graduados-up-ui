0a2a4ccabcdbf046a835dd2c169b95e496b78005
David Murillo <david.josue.murillo23@gmail.com>
2025-01-03 20:40:42 -0500

    docs: :memo: Writing about project

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,21 +5,21 @@ import TextSection from './components/TextSection';
 import ProfileSection from './components/ProfileSections';
 
 const About = () => {
-  const { introContent, profileDescription } = ABOUT_CONTENT;
+    const { introContent, profileDescription } = ABOUT_CONTENT;
 
-  return (
-      <main className="mx-2 md:mx-16 xl:container lg:mx-auto px-6 py-12 mt-20">
-        <TextSection
-          title="¿Qué es Lorem Ipsum?"
-          content={introContent}
-        />
-        <ProfileSection
-          image={mephoto}
-          title="¿Quien Soy?"
-          description={profileDescription}
-        />
-      </main>
-  );
+    return (
+        <main className="mx-2 md:mx-16 xl:container lg:mx-auto px-6 py-12 mt-20">
+            <TextSection
+                title="¿Qué es Graduados UP?"
+                content={introContent}
+            />
+            <ProfileSection
+                image={mephoto}
+                title="¿Quien Soy?"
+                description={profileDescription}
+            />
+        </main>
+    );
 };
 
 export default About;
--- src/pages/About/constants/index.js
+++ src/pages/About/constants/index.js
@@ -1,7 +1,7 @@
 export const ABOUT_CONTENT = {
     introContent: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare odio nec neque mollis scelerisque a eu turpis. Duis neque lorem, condimentum sed metus ac, tristique mattis odio. Ut in est vel diam faucibus mattis vitae non ligula.",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      "Graduados UP es un sistema de gestión de estudiantes graduados de la Universidad de Panamá que abarca todas las facultades, carreras y campus de la institución. Este sistema permite a los estudiantes consultar datos como la cantidad de graduados por año, facultad, carrera, campus, entre otros indicadores relevantes",
+      "Este proyecto nacio con el objetivo de facilitar la consulta de datos de graduados de la Universidad de Panamá."
     ],
     profileDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare odio nec neque mollis scelerisque a eu turpis. Duis neque lorem, condimentum sed metus ac, tristique mattis odio. Ut in est vel diam dipiscing elit. Sed ornare odio nec neque mollis scelerisque a eu turpis."
   };
