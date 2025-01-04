20996fcd5c58fff69e77c87f019403515c19c9f3
David Murillo <david.josue.murillo23@gmail.com>
2025-01-03 20:44:01 -0500

    docs: :memo: Writing about me

--- src/pages/About/constants/index.js
+++ src/pages/About/constants/index.js
@@ -3,5 +3,5 @@ export const ABOUT_CONTENT = {
       "Graduados UP es un sistema de gestión de estudiantes graduados de la Universidad de Panamá que abarca todas las facultades, carreras y campus de la institución. Este sistema permite a los estudiantes consultar datos como la cantidad de graduados por año, facultad, carrera, campus, entre otros indicadores relevantes",
       "Este proyecto nacio con el objetivo de facilitar la consulta de datos de graduados de la Universidad de Panamá."
     ],
-    profileDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare odio nec neque mollis scelerisque a eu turpis. Duis neque lorem, condimentum sed metus ac, tristique mattis odio. Ut in est vel diam dipiscing elit. Sed ornare odio nec neque mollis scelerisque a eu turpis."
+    profileDescription: "Soy David Murillo, estudiante de la Universidad de Panamá, actualmente cursando la Licenciatura en Informática para la Gestión Educativa y Empresarial, actualmnete especializandome en el desarrollo backend. Soy un apasionado por la tecnología y el desarrollo de software. Me encanta aprender cosas nuevas y compartir mis conocimientos con los demás."
   };
