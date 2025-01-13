d80e434e8fdce79d3c384b8a7430c7b1cab59474
David Murillo <david.josue.murillo23@gmail.com>
2025-01-12 20:58:25 -0500

    refactor: :art: Making content dynamic

--- src/components/YearlySummary.jsx
+++ src/components/YearlySummary.jsx
@@ -3,11 +3,17 @@ const YearlySummary = ({ yearlyTotals }) => {
 
     return (
         <div className="mt-4">
-            <h3 className="text-yellow-600 text-xl font-bold mb-2">Totales por Año:</h3>
+            {
+                sortedYears.length < 5 ? (
+                    <h3 className="text-yellow-600 text-xl font-bold mb-2">Totales por Año:</h3>
+                ) : (
+                    <h3 className="text-yellow-600 text-xl font-bold mb-2">Totales por Facultad (últimos 5 años):</h3>
+                )
+            }
             <ul className="dark:text-gray-200 flex justify-between flex-wrap gap-x-4 px-4">
                 {sortedYears.map(([year, total]) => (
                     <li key={year} className="mb-1 list-disc mx-2">
-                        Año {year}: <span className="text-green-700 dark:text-green-500">{total}</span> graduados
+                        {year}: <span className="text-green-700 dark:text-green-500">{total}</span> graduados
                     </li>
                 ))}
             </ul>
