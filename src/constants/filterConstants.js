a5479dff5556b2f1d8c7092fe5266b9c82f4c7ba
David Murillo <david.josue.murillo23@gmail.com>
2025-01-12 19:32:06 -0500

    refactor: :wrench: Implementing functional filters

--- src/constants/filterConstants.js
+++ src/constants/filterConstants.js
@@ -2,7 +2,9 @@
 export const FILTER_CONDITIONS = {
     '2019-2022': () => true,
     'Ingeniería en Informática': (item) => item?.career?.name === 'Lic. en Ingeniera en Informática',
-    'faeco': (item) => item?.career.faculty.name === 'Administración de Empresas y Contabilidad',
+    'faeco': (item) => item?.career?.faculty?.name === 'Administración de Empresas y Contabilidad',
+    'up central': (item) => item?.campus?.id == 1,
+    'cruv': (item) => item?.campus?.id == 11,
     // Puedes agregar más filtros aquí
 };
 
--- src/pages/Home/constants/graduates.js
+++ src/pages/Home/constants/graduates.js
@@ -8,9 +8,9 @@ export const FILTER_OPTIONS = [
     { id: 1, label: 'Graduados entre 2019-2022', value: '2019-2022' },
     { id: 2, label: 'Graduados de Ingeniería en Informática', value: 'Ingeniería en Informática' },
     { id: 3, label: 'Graduados de Fac. en Administración  de Empresas', value: 'faeco' },
-    { id: 4, label: 'Graduados en Ingeniería', value: 'ingenieria' },
-    { id: 5, label: 'Graduados en Ciencias', value: 'ciencias' },
-    { id: 6, label: 'Graduados en Humanidades', value: 'humanidades' }
+    { id: 4, label: 'Graduados en la sede principal', value: 'up central' },
+    { id: 5, label: 'Graduados en CRU Veraguas', value: 'cruv' },
+    { id: 6, label: 'Graduados por facultad', value: 'humanidades' }
 ];
 
 export const INFO_CARDS = [
